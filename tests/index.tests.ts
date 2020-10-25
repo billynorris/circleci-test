ef740a61279b2ce1a33f29fabde7ccd05295b724
Billy Norris <billy@Billys-MacBook-Pro.local>
2020-10-25 12:35:16 +0000

    Added simple test

---
+++ tests/index.tests.ts
@@ -0,0 +1,18 @@
+import { expect } from 'chai'
+
+
+describe("Index tests", async () => {
+
+    beforeEach(() => {
+
+    });
+
+    afterEach(() => {
+    });
+
+
+    it("Can calculate 1 + 1", async () => {
+        expect(1 === 1)
+    });
+
+});
